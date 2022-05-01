10ce373e96d7f4a4cbade4fbbe8c54471e238253
Tripan <roytripandipta@gmail.com>
2022-05-01 08:52:58 +0530

    add

--- invoice.php
+++ invoice.php
@@ -2,6 +2,60 @@
 //call the FPDF library
 
 require('fpdf17/fpdf.php');
+class PDF extends FPDF
+{
+    function Footer()
+    {
+        // Go to 1.5 cm from bottom
+        $this->SetY(-15);
+        // Select Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Print centered page number
+        $this->Cell(0,10,'Page '.$this->PageNo(),0,0,'C');
+    }
+}
+
+class PDF1 extends PDF
+{
+    function RoundedRect($x, $y, $w, $h, $r, $style = '')
+    {
+        $k = $this->k;
+        $hp = $this->h;
+        if($style=='F')
+            $op='f';
+        elseif($style=='FD' || $style=='DF')
+            $op='B';
+        else
+            $op='S';
+        $MyArc = 4/3 * (sqrt(2) - 1);
+        $this->_out(sprintf('%.2F %.2F m',($x+$r)*$k,($hp-$y)*$k ));
+        $xc = $x+$w-$r ;
+        $yc = $y+$r;
+        $this->_out(sprintf('%.2F %.2F l', $xc*$k,($hp-$y)*$k ));
+
+        $this->_Arc($xc + $r*$MyArc, $yc - $r, $xc + $r, $yc - $r*$MyArc, $xc + $r, $yc);
+        $xc = $x+$w-$r ;
+        $yc = $y+$h-$r;
+        $this->_out(sprintf('%.2F %.2F l',($x+$w)*$k,($hp-$yc)*$k));
+        $this->_Arc($xc + $r, $yc + $r*$MyArc, $xc + $r*$MyArc, $yc + $r, $xc, $yc + $r);
+        $xc = $x+$r ;
+        $yc = $y+$h-$r;
+        $this->_out(sprintf('%.2F %.2F l',$xc*$k,($hp-($y+$h))*$k));
+        $this->_Arc($xc - $r*$MyArc, $yc + $r, $xc - $r, $yc + $r*$MyArc, $xc - $r, $yc);
+        $xc = $x+$r ;
+        $yc = $y+$r;
+        $this->_out(sprintf('%.2F %.2F l',($x)*$k,($hp-$yc)*$k ));
+        $this->_Arc($xc - $r, $yc - $r*$MyArc, $xc - $r*$MyArc, $yc - $r, $xc, $yc - $r);
+        $this->_out($op);
+    }
+
+    function _Arc($x1, $y1, $x2, $y2, $x3, $y3)
+    {
+        $h = $this->h;
+        $this->_out(sprintf('%.2F %.2F %.2F %.2F %.2F %.2F c ', $x1*$this->k, ($h-$y1)*$this->k,
+            $x2*$this->k, ($h-$y2)*$this->k, $x3*$this->k, ($h-$y3)*$this->k));
+    }
+}
 // require(include/fpdf153/fpdf.php)
 
 //A4 width : 219mm
@@ -10,15 +64,26 @@ require('fpdf17/fpdf.php');
 
 //create pdf object
 $phone_number = $_GET['phone_number'];
-$pdf = new FPDF('P','mm','A4');
+$pdf = new PDF1('P','mm','A4');
 //add new page
 $pdf->AddPage();
+
 $pdf->SetFont('Arial','B',14);
-$pdf->Image('ASPIRE.png',$pdf->GetX(),$pdf->GetY(), 35, 15);
+$pdf->Image('aspire.png',$pdf->GetX(),$pdf->GetY(), 35, 20);
 //Cell(width , height , text , border , end line , [align] )
 $pdf->SetFont('Arial','B',15);
 $pdf->Cell(190 ,5,'Aspire Digital Credit Card',0,1,'C');
 $pdf->Cell(59 ,5,' ',0,1);//end of line
+
+// $x = $pdf->GetX();
+// $y = $pdf->GetY();
+
+// $pdf -> Rect($x+80,$y,10,10,'D');
+$x = $pdf->GetPageWidth()/2;
+$y = $pdf->GetY();
+$pdf->SetLineWidth(0.5);
+$pdf -> Rect($x-30,$y-2,60,9,'D');
+$pdf->SetLineWidth(0.2);
 $pdf->Cell(190 ,5,'April 2021 Statement',0,1,'C');//end of line
 $pdf->SetFont('Arial','B',11);
 
@@ -55,6 +120,7 @@ $payment_min = 0;
 $payment_max = 0;
 $amount_carry_forward = 0;
 $link = "";
+$due_date="";
  // $handle = fopen('D:/aspire_crons/Billing Automation/february2021_automated_bills.csv', 'r');
 // $handle = fopen('bills_to_be_generated.csv', 'r');
 //           while(($data = fgetcsv($handle)) !== FALSE) {
@@ -76,7 +142,7 @@ $link = "";
 //             }
 //           }
 // fclose($handle);
-$sql_2 = "select * from billing_info where user_id = '{$id}'";
+$sql_2 = "select * from billing_info where user_id = '{$id}' and billing_month='July'";
 $result2 = $conn->query($sql_2);
 if($result2 -> num_rows > 0) {
 	while($row = $result2->fetch_assoc()) {
@@ -93,46 +159,79 @@ if($result2 -> num_rows > 0) {
 		$payment_max = $row["payment_max"];
 		$amount_carry_forward = $row["amount_carry_forward"];
 		$link = $row["payment_link"];
+		$due_date = $row['due_date'];
+		break;
 	}
 }
 
 if($result-> num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $pdf->Cell(59 ,10,' ',0,1);//end of line
+        $pdf->Cell(59 ,20,' ',0,1);//end of line
+		$x1 = $pdf->GetX();
+		$y1 = $pdf->GetY();
+
 		$pdf->Cell(100, 5, 'To,',0,1);
-		$pdf->Cell(59 ,5,' ',0,1);//end of line
+		$pdf->Cell(59 ,3,' ',0,1);//end of line
 		$pdf->SetFont('Arial','B',11);
+
 		$pdf->Cell(120,5,$row['user_name'],0,0);
 		$x = $pdf->GetX();
 		$y = $pdf->GetY();
 		$pdf->SetXY($x, $y - 5);
 		$pdf->SetFont('Arial','B',15);
-		$pdf->Cell(60,5,'Due Amount: '.$pdf->Image('inr.jpg',$pdf->GetX()+35,$pdf->GetY(), 3, 3).'  '.number_format($payment_min,2,'.',','), 0, 1);
+		$pdf->SetTextColor(178,9,9);
+		$pdf->Cell(60,5,'Due Amount: '.$pdf->Image('inr.png',$pdf->GetX()+35,$pdf->GetY(), 3, 5).'  '.number_format($payment_min,2,'.',','), 0, 1);
 		$x = $pdf->GetX();
 		$y = $pdf->GetY();
 		$pdf->SetXY($x, $y + 5);
 		$pdf->SetFont('Arial','B',11);
+		$pdf->SetTextColor(0,0,0);
 		$x = $pdf->GetX();
 		$y = $pdf->GetY();
 		$pdf->MultiCell(100,8,trim($row['address']).' '.$row['pin_code'],0,0);
-		$pdf->SetXY($x + 120, $y);
-		$pdf->Cell(60,5,'Due Date: 8 May 2021', 0, 1);
+		$pdf->SetXY($x + 120, $y-2);
+		$pdf->SetFont('Arial','B',15);
+		$pdf->SetTextColor(178,9,9);
+		$pdf->Cell(60,5,'Due Date: '.$due_date, 0, 1);
+		$pdf->SetTextColor(0,0,0);
+		$pdf->SetFont('Arial','B',11);
 		$x = $pdf->GetX();
 		$y = $pdf->GetY();
-		$pdf->SetXY($x + 120, $y);
-		$pdf->MultiCell(60,5,'Late Charges:  300 + GST for payments after 8 May 2021',0,1);
-		$pdf->Cell(120,10,'Phone: '.$row['phone_number'],0,0);
+		$pdf->SetXY($x + 120, $y+2);
+		$late_fee=301;
+		if ($late_fee == 0) {
+			$pdf->MultiCell(60,5,'Late Charges: 300 + GST for payments after '.$due_date,0,1);
+		}
+		else {
+			$pdf->MultiCell(60,5,'Late Charges: '.$late_fee.' + GST for payments after '.$due_date,0,1);
+		}
+		$pdf->Cell(120,10,'Phone: '.$row['phone_number'],0,1);
+
+		$x2 = $pdf->GetX();
+		$y2 = $pdf->GetY();
+		$pdf->SetLineWidth(0.5);
+		$pdf -> RoundedRect($x1-1,$y1-2,$pdf->GetPageWidth()/2,$y2-$y1+15,1.5,'D');
+		$pdf -> RoundedRect($x1+$pdf->GetPageWidth()/2+10,$y1-2,$pdf->GetPageWidth()/2-30,$y2-$y1,1.5,'D');
+		$pdf->SetLineWidth(0.2);
+		$pdf->Cell(59 ,5,' ',0,1);//end of line
 		// $link = 'https://stackoverflow.com/questions/54644886/insert-a-url-inside-a-cell-in-fpdf';
 		// $pdf->SetFont('Arial','U',20);
-		$pdf->SetFont('Arial','BU',20);
-		$pdf->SetTextColor(194,8,8);
+		$pdf->SetFont('Arial','BU',22);
+		// $pdf->SetTextColor(194,8,8);
+		$pdf->SetTextColor(0,0,0);
 		$x = $pdf->GetX();
 		$y = $pdf->GetY();
-		$pdf->SetXY($x, $y+4);
-		$pdf->Cell(45,12,'Pay Now','0','1','C',false, $link);
+		$pdf->setFillColor(245,190,67);
+		$pdf -> Rect(0,$y+17,$pdf->GetPageWidth(),19,'F');
+		// $pdf->SetXY($x-50, $y+20);
+		$pdf->SetXY($x, $y+20);
+		$pdf->SetTextColor(178,9,9);
+		$pdf->Cell($pdf->GetPageWidth()-10,12,'Click here to Pay Now','0','1','C',false, $link);
+		$pdf->SetTextColor(0,0,0);
 
 		$pdf->SetTextColor(0,0,0);
 		$pdf->SetFont('Arial','B',11);
+		$pdf->setFillColor(255,255,255);
 
 		$pdf->Cell(59 ,10,' ',0,1);//end of line
 
@@ -142,25 +241,25 @@ if($result-> num_rows > 0) {
 		$pdf->Line($x,$y,$x + $x+120, $y);
 		$pdf->Cell(59 ,8,' ',0,1);//end of line
 		$pdf->Cell(120,8, 'Due EMI this month', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($due_emi_this_month,2,'.',','), 1, 1,'C');
-
+		// $pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($due_emi_this_month,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($due_emi_this_month,2,'.',','), 1, 1,'C');
 		$pdf->Cell(120,8, 'Monthly Membership Fee', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($monthly_fee,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($monthly_fee,2,'.',','), 1, 1,'C');
 
 		$pdf->Cell(120,8, 'GST on Membership Fee', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($monthly_tax,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($monthly_tax,2,'.',','), 1, 1,'C');
 
 		$pdf->Cell(120,8, 'Late Fee', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($late_fee,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($late_fee,2,'.',','), 1, 1,'C');
 
 		$pdf->Cell(120,8, 'GST on Late Fee', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($late_tax,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($late_tax,2,'.',','), 1, 1,'C');
 
 		$pdf->Cell(120,8, 'Excess Payment Carry Forward', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($amount_carry_forward,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($amount_carry_forward,2,'.',','), 1, 1,'C');
 
 		$pdf->Cell(120,8, 'Net Due', 1, 0);
-		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+22,$pdf->GetY()+2, 3, 3).'     '.number_format($payment_min,2,'.',','), 1, 1,'C');
+		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+20,$pdf->GetY()+2, 3, 3).'     '.number_format($payment_min,2,'.',','), 1, 1,'C');
 
 		$pdf->Cell(59 ,8,' ',0,1);//end of line
 
@@ -177,21 +276,27 @@ if($result-> num_rows > 0) {
 		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+2,$pdf->GetY()+2, 3, 3).'     '.number_format($sanction_amount,2,'.',','), 1, 0,);
 		$pdf->Cell(60,8, $pdf->Image('inr.jpg',$pdf->GetX()+2,$pdf->GetY()+2, 3, 3).'     '.number_format($available_limit,2,'.',','), 1, 0,);
 		$pdf->Cell(60,8, $DPD ,1, 1,);
+		break ;
     }
 }
 
 // $pdf->Cell(10,5,number_format($amt,2,'.',','), 0,1);
 
 $pdf->Cell(60,8,' ', 0, 1);
-
+$x1 = $pdf->GetX();
+$y1 = $pdf->GetY();
 if($DPD == 0)
 {
-	$pdf->MultiCell(180,8,'Congratulations! on improving your credit score (all bureaus including CIBIL) with on time payment before or on 8th May.',1,1);
+	$pdf->MultiCell(180,8,'Congratulations! on improving your credit score (all bureaus including CIBIL) with on time payment before or on 8th May.',0,1);
 }else {
-	$pdf->MultiCell(180,8,'New year brings new hope! Please pay your dues to make the new year financially happy!',1,1);
+	$pdf->MultiCell(180,8,'New year brings new hope! Please pay your dues to make the new year financially happy!',0,1);
 }
+$x2 = $pdf->GetX();
+$y2 = $pdf->GetY();
 
-$pdf->Cell(60,10,' ', 0, 1);
+$pdf->RoundedRect($x1-1, $y1-1, 182, $y2-$y1+2, 2, 'D');
+$pdf->AddPage();
+// $pdf->Cell(60,10,' ', 0, 1);
 $pdf->Cell(60,10,'Transaction History', 0,1);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
@@ -212,7 +317,8 @@ $sql = "SELECT
       merchant_details AS m1 ON m.merchant_id = m1.merchant_id
   WHERE
       m.user_id = '{$id}'
-          AND m.`status` = 'success';";
+          AND m.`status` = 'success'
+	order by m.created_time desc;";
 $result1 = $conn->query($sql);
 
 if($result1-> num_rows > 0) {
@@ -225,34 +331,138 @@ if($result1-> num_rows > 0) {
 }
 
 
-$pdf->Cell(60,10,' ', 0, 1);
-$pdf->Cell(60,10,'Payment History', 0,1);
+// $pdf->Cell(60,10,' ', 0, 1);
+$pdf->AddPage();
+$pdf->Cell(60,10,'Repayment History', 0,1);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
 $pdf->Line($x,$y,$x+120, $y);
 $pdf->Cell(59 ,5,' ',0,1);//end of line
-$pdf->Cell(60,8,'Payment Time', 1, 0);
+$pdf->Cell(60,8,'Repayment Time', 1, 0);
 $pdf->Cell(60,8,'Amount', 1, 1);
 
 $payment_time = "2021-05-01 00:00:00";
 $sql = "SELECT *
 from (select user_id, amount, substr(created_time,1,10) as payment_time from pay_now_summary where user_id = '{$id}' and `status` = 'success'
-union all select user_id, amount, substr(payment_time,1,10) as payment_time from repayment_details where user_id = '{$id}') tbl;";
+union all select user_id, amount, substr(payment_time,1,10) as payment_time from repayment_details where user_id = '{$id}') tbl 
+order by payment_time desc;";
 $result1 = $conn->query($sql);
 if($result1-> num_rows > 0) {
     while($row = $result1->fetch_assoc()) {
 
         $pdf->Cell(60,8,$row['payment_time'], 1, 0);
 		$pdf->Cell(60,8,number_format($row['amount'],2,'.',','), 1, 1);
+
+    }
+}
+
+$pdf->Cell(60,10,' ', 0, 1);
+$pdf->Cell(60,10,'Fee Payment Details', 0,1);
+$x = $pdf->GetX();
+$y = $pdf->GetY();
+$pdf->Line($x,$y,$x+120, $y);
+$pdf->Cell(59 ,5,' ',0,1);//end of line
+$pdf->Cell(45,8,'Billing Month', 1, 0);
+$pdf->Cell(45,8,'Fee Type', 1, 0);
+$pdf->Cell(45,8,'Fee Amount', 1, 0);
+$pdf->Cell(45,8,'Status', 1, 1);
+
+// write sql query here
+$sql = "SELECT
+date_format(date_sub(billed_date, interval 1 month), '%b %Y') as billing_month,
+fee_type,
+fee_billed as amount,
+(case when fl_paid = -1 then 'waived'
+when fl_paid = 0 then 'charged'
+when fl_paid = 1 then 'paid'
+end) as status
+FROM
+fee_payment_details where user_id='{$id}'
+order by billed_date desc;";
+$result1 = $conn->query($sql);
+if($result1-> num_rows > 0) {
+    while($row = $result1->fetch_assoc()) {
+
+        $pdf->Cell(45,8,$row['billing_month'], 1, 0);
+		$pdf->Cell(45,8,$row['fee_type'], 1, 0);
+		$pdf->Cell(45,8,number_format($row['amount'],2,'.',','), 1, 0);
+		$pdf->Cell(45,8,$row['status'], 1, 1);
         
     }
 }
+
+$pdf->AddPage();
+
+// $pdf->Cell(60,10,' ', 0, 1);
+$pdf->Cell(60,10,'Refund Status Details', 0,1);
+$x = $pdf->GetX();
+$y = $pdf->GetY();
+$pdf->Line($x,$y,$x+120, $y);
+$pdf->Cell(59 ,5,' ',0,1);//end of line
+
+$pdf->SetFont('Arial','B',10);
+$pdf->Cell(29,8,'Date of Txn', 1, 0);
+$pdf->Cell(35,8,'Downpayment Amt', 1, 0);
+$pdf->Cell(30,8,'Refund Date', 1, 0);
+$pdf->Cell(28,8,'Refund Amt', 1, 0);
+$pdf->Cell(28,8,'Refund Status', 1, 0);
+$pdf->Cell(30,8,'Reference ID', 1, 1);
+
+// write sql query here
+$sql = "SELECT 
+STR_TO_DATE(m.created_time, '%Y-%m-%d') AS `date_of_txn`,
+d.amount as `down_payment_amount`,
+str_to_date(r.refund_date, '%Y-%m-%d') as `refund_date`,
+r.amount as `refund_amount`,
+r.refund_status as `refund_status`,
+r.asp_request_id as `refund_reference_id`
+FROM
+down_payment_details d
+	LEFT JOIN
+merchant_payments m ON d.txn_id = m.down_payment_transaction_id
+left join 
+refund_payment_details r
+on
+d.txn_id = r.down_payment_transaction_id
+WHERE
+d.user_id='{$id}'
+and
+d.status = 'success'
+	AND (m.status <> 'success'
+	OR m.status IS NULL)
+	order by m.created_time desc";
+
+$result1 = $conn->query($sql);
+if($result1-> num_rows > 0) {
+    while($row = $result1->fetch_assoc()) {
+
+        $pdf->Cell(29,8,$row['date_of_txn'], 1, 0);
+		$pdf->Cell(35,8,number_format($row['down_payment_amount'],2,'.',','), 1, 0);
+		$pdf->Cell(30,8,$row['refund_date'], 1, 0);
+		$pdf->Cell(28,8,number_format($row['refund_amount'],2,'.',','), 1, 0);
+		$pdf->Cell(28,8,$row['refund_status'], 1, 0);
+		$pdf->Cell(30,8,$row['refund_reference_id'], 1, 1);
+        
+    }
+}
+
+$pdf->SetFont('Arial','B',11);
 $conn->close();
+
 $pdf->Cell(60,10,' ', 0, 1);
 $x = $pdf->GetX();
 $y = $pdf->GetY();
-$pdf->Line($x,$y,$x+180, $y);
-$pdf->Cell(60,5,' ', 0, 1);
+
+if($y + 100 > $pdf->GetPageHeight()) {
+	$pdf->AddPage();
+}
+else {
+	$pdf->Line($x,$y,$x+180, $y);
+    $pdf->Cell(60,5,' ', 0, 1);
+}
+$x1= $pdf->GetX();
+$y1 = $pdf->GetY();
+
 $pdf->Cell(80,8,'Aspire Fintech Private Limited', 0, 0);
 $pdf->Cell(80,8,'Our Address', 0, 1);
 
@@ -262,17 +472,29 @@ $y = $pdf->GetY();
 $pdf->MultiCell(80,8,'3RD FLOOR, H-0302, SMONDO-3, NEOTOWN ROAD,
 HULIMAMGALA VILLAGE, JIGANI HOBLI,
 Bengaluru, Karnataka, 560099', 0, 1);
-
-$mail = "mailto:contact@letsaspire.in";
-$pdf->Cell(45,8,'Email: contact@letsaspire.in','0','1','',false, $mail);
+$x2 = $pdf->GetX();
+$y2 = $pdf->GetY();
+$pdf->SetXY($x2+80, $y2);
 $whatsapp = "https://wa.me/918431568414/?text=hii";
 $pdf->Cell(45,8,'Whatsapp: +91 8431568414','0','1','',false, $whatsapp);
+
+$pdf->SetLineWidth(0.5);
+
+$pdf->RoundedRect($x1-1, $y1-1, 70, $y2-$y1-10, 2, 'D');
+$pdf->RoundedRect($x1+78, $y1-1, 84, $y2-$y1+12, 2, 'D');
+$pdf->SetLineWidth(0.2);
+
+// $mail = "mailto:contact@letsaspire.in";
+// $pdf->Cell(45,8,'Email: contact@letsaspire.in','0','1','',false, $mail);
+// $whatsapp = "https://wa.me/918431568414/?text=hii";
+// $pdf->Cell(45,8,'Whatsapp: +91 8431568414','0','1','',false, $whatsapp);
 $pdf->SetXY($x, $y+8);
 $pdf->Cell(80,8,'PAN: AATCA6761C', 0, 1);
-
+$month = "June";
+$year = "2021";
 
 //output the result
 ob_start();
-$pdf->Output();
+$pdf->Output('I', $month.' '.$year. ' '.'file_name_2.pdf');
  
 ?>
